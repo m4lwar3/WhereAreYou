a667f91c53c003d7a3a900c1bb04914165116864
Athavan <aadhavan.ravichandran@gmail.com>
2013-02-06 15:23:25 +0530

    Change-Id: Id9c54d7e16ac52ef987d1fb6680dc9d930e1ba79

--- src/com/example/whereareyou/MainActivity.java
+++ src/com/example/whereareyou/MainActivity.java
@@ -1,4 +1,4 @@
-package com.aadhavan.whereareyou;
+package com.example.whereareyou;
 
 import com.example.whereareyou.R;
 import com.google.android.maps.MapActivity;
@@ -10,15 +10,12 @@ public class MainActivity extends MapActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 	@Override
